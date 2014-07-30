a435fa4b67127e9d206880a8c4799b5d61be459c
Ilya Dmitrichenko <ilya@xively.com>
2014-07-30 21:19:56 +0100

    Organise code and add Travis CI config

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ semver
 ======
 
 Semantic version parsing and comparison.
+
+[![Build Status](https://travis-ci.org/rust-lang/semver.svg?branch=master)](https://travis-ci.org/rust-lang/semver)
--- src/lib.rs
+++ src/lib.rs
@@ -34,8 +34,7 @@
 #![crate_type = "dylib"]
 #![license = "MIT/ASL2"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
-       html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-       html_root_url = "http://doc.rust-lang.org/master/")]
+       html_favicon_url = "http://www.rust-lang.org/favicon.ico")]
 #![feature(default_type_params)]
 
 use std::char;
