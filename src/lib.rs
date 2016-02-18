21419efc6f959a80357572fbb9e0237520fc2980
Kamal Marhubi <kamal@marhubi.com>
2016-02-18 12:41:10 -0500

    Export SemVerError in the public interface
    This appears to have been missed in 5b7a7f8.

--- src/lib.rs
+++ src/lib.rs
@@ -168,7 +168,7 @@ extern crate nom;
 // We take the common approach of keeping our own module system private, and
 // just re-exporting the interface that we want.
 
-pub use version::{Version, Identifier};
+pub use version::{Version, Identifier, SemVerError};
 pub use version::Identifier::{Numeric, AlphaNumeric};
 pub use version_req::{VersionReq, ReqParseError};
 
