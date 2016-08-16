2e075712a1a62bcb1689b1c0e8cd77b50eeb61b9
Steve Klabnik <steve@steveklabnik.com>
2016-08-16 15:47:25 -0400

    log it

--- tests/regression.rs
+++ tests/regression.rs
@@ -20,10 +20,12 @@ fn test_regressions() {
     for krate in index.crates() {
         for version in krate.versions() {
             let v = version.version();
+            println!("testing {}", v);
             assert!(Version::parse(v).is_ok(), "failed: {} ({})", version.name(), v);
 
             for dependency in version.dependencies() {
                 let r = dependency.requirement();
+                println!("testing dependency: {}", r);
                 assert!(VersionReq::parse(r).is_ok(), "failed: {} ({})", dependency.name(), r);
             }
         }
