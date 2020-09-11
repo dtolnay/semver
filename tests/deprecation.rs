e7043d2973a7684fd55727e99ad65e6372a7725a
Terence Lee <hone02@gmail.com>
2020-09-10 19:07:02 -0500

    VersionReq parses as '*' now, so remove this deprecation.
    See discussion here: https://github.com/steveklabnik/semver/pull/209#discussion_r459465532

--- tests/deprecation.rs
+++ tests/deprecation.rs
@@ -10,8 +10,6 @@ fn test_regressions() {
         ("0.1.0.", VersionReq::parse("0.1.0").unwrap()),
         ("0.3.1.3", VersionReq::parse("0.3.13").unwrap()),
         ("0.2*", VersionReq::parse("0.2.*").unwrap()),
-        // TODO: this actually parses as '*' now, not sure if that's OK
-        // ("*.0", VersionReq::any()),
     ];
 
     for (version, requirement) in versions.into_iter() {
