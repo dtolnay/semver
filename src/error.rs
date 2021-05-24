c976dc537af0f94035d15ea6aa2092de66d629d0
David Tolnay <dtolnay@gmail.com>
2021-05-24 01:57:54 -0700

    Add identifier parser

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,56 @@
 use crate::parse::Error;
 use std::fmt::{self, Debug, Display};
 
+pub(crate) enum ErrorKind {
+    LeadingZero(Position),
+    EmptySegment(Position),
+    IllegalCharacter(Position),
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub(crate) enum Position {
+    Pre,
+    Build,
+}
+
 impl std::error::Error for Error {}
 
 impl Display for Error {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        let _ = formatter;
-        unimplemented!()
+        match &self.kind {
+            ErrorKind::LeadingZero(pos) => {
+                formatter.write_str("invalid leading zero in ")?;
+                Display::fmt(pos, formatter)?;
+                Ok(())
+            }
+            ErrorKind::EmptySegment(pos) => {
+                formatter.write_str("empty identifier segment in ")?;
+                Display::fmt(pos, formatter)?;
+                Ok(())
+            }
+            ErrorKind::IllegalCharacter(pos) => {
+                formatter.write_str("unexpected character in ")?;
+                Display::fmt(pos, formatter)?;
+                Ok(())
+            }
+        }
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(match self {
+            Position::Pre => "pre-release identifier",
+            Position::Build => "build metadata",
+        })
     }
 }
 
 impl Debug for Error {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        let _ = formatter;
-        unimplemented!()
+        formatter.write_str("Error(\"")?;
+        Display::fmt(self, formatter)?;
+        formatter.write_str("\")")?;
+        Ok(())
     }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,10 @@
+use crate::error::{ErrorKind, Position};
+use crate::identifier::Identifier;
 use crate::{BuildMetadata, Comparator, Prerelease, Version, VersionReq};
 use std::str::FromStr;
 
 pub struct Error {
-    _todo: (),
+    pub(crate) kind: ErrorKind,
 }
 
 impl FromStr for Version {
@@ -36,8 +38,11 @@ impl FromStr for Prerelease {
     type Err = Error;
 
     fn from_str(text: &str) -> Result<Self, Self::Err> {
-        let _ = text;
-        unimplemented!()
+        let (pre, rest) = prerelease_identifier(text)?;
+        if !rest.is_empty() {
+            return Err(Error::new(ErrorKind::IllegalCharacter(Position::Pre)));
+        }
+        Ok(pre)
     }
 }
 
@@ -45,7 +50,70 @@ impl FromStr for BuildMetadata {
     type Err = Error;
 
     fn from_str(text: &str) -> Result<Self, Self::Err> {
-        let _ = text;
-        unimplemented!()
+        let (build, rest) = build_identifier(text)?;
+        if !rest.is_empty() {
+            return Err(Error::new(ErrorKind::IllegalCharacter(Position::Build)));
+        }
+        Ok(build)
+    }
+}
+
+impl Error {
+    fn new(kind: ErrorKind) -> Self {
+        Error { kind }
+    }
+}
+
+fn prerelease_identifier(input: &str) -> Result<(Prerelease, &str), Error> {
+    let (string, rest) = identifier(input, Position::Pre)?;
+    let identifier = unsafe { Identifier::new_unchecked(string) };
+    Ok((Prerelease { identifier }, rest))
+}
+
+fn build_identifier(input: &str) -> Result<(BuildMetadata, &str), Error> {
+    let (string, rest) = identifier(input, Position::Build)?;
+    let identifier = unsafe { Identifier::new_unchecked(string) };
+    Ok((BuildMetadata { identifier }, rest))
+}
+
+fn identifier(input: &str, pos: Position) -> Result<(&str, &str), Error> {
+    let mut accumulated_len = 0;
+    let mut segment_len = 0;
+    let mut segment_has_nondigit = false;
+
+    loop {
+        match input.as_bytes().get(accumulated_len + segment_len) {
+            Some(b'A'..=b'Z') | Some(b'a'..=b'z') | Some(b'-') => {
+                segment_len += 1;
+                segment_has_nondigit = true;
+            }
+            Some(b'0'..=b'9') => {
+                segment_len += 1;
+            }
+            boundary => {
+                if segment_len == 0 {
+                    if accumulated_len == 0 && boundary != Some(&b'.') {
+                        return Ok(("", input));
+                    } else {
+                        return Err(Error::new(ErrorKind::EmptySegment(pos)));
+                    }
+                }
+                if pos == Position::Pre
+                    && segment_len > 1
+                    && !segment_has_nondigit
+                    && input[accumulated_len..].starts_with('0')
+                {
+                    return Err(Error::new(ErrorKind::LeadingZero(pos)));
+                }
+                accumulated_len += segment_len;
+                if boundary == Some(&b'.') {
+                    accumulated_len += 1;
+                    segment_len = 0;
+                    segment_has_nondigit = false;
+                } else {
+                    return Ok(input.split_at(accumulated_len));
+                }
+            }
+        }
     }
 }
