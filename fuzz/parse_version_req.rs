bae08d4a88444894fe435c7c62f9370fdfeefce6
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-05-25 23:03:30 +0300

    extend fuzzing to cover Display as well

--- fuzz/parse_version_req.rs
+++ fuzz/parse_version_req.rs
@@ -6,6 +6,9 @@ use std::str;
 
 fuzz_target!(|bytes: &[u8]| {
     if let Ok(string) = str::from_utf8(bytes) {
-        let _ = VersionReq::parse(string);
+        if let Ok(v1) = VersionReq::parse(string) {
+            let v2 = VersionReq::parse(&v1.to_string()).unwrap();
+            assert_eq!(v1, v2);
+        }
     }
 });
