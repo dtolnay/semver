324ffce5d914778062136c9744ffdf53523c9fa2
David Tolnay <dtolnay@gmail.com>
2025-12-19 21:50:49 -0800

    Switch from cargo bench to criterion

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,25 +1,21 @@
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{criterion_group, criterion_main, Criterion};
 use semver::{Prerelease, Version, VersionReq};
 use std::hint;
-use test::Bencher;
 
-#[bench]
-fn parse_prerelease(b: &mut Bencher) {
-    let text = "x.7.z.92";
-    b.iter(|| hint::black_box(text).parse::<Prerelease>().unwrap());
+fn bench(c: &mut Criterion) {
+    c.bench_function("parse Prerelease", |b| {
+        let text = "x.7.z.92";
+        b.iter(|| hint::black_box(text).parse::<Prerelease>().unwrap());
+    });
+    c.bench_function("parse Version", |b| {
+        let text = "1.0.2021-beta+exp.sha.5114f85";
+        b.iter(|| hint::black_box(text).parse::<Version>().unwrap());
+    });
+    c.bench_function("parse VersionReq", |b| {
+        let text = ">=1.2.3, <2.0.0";
+        b.iter(|| hint::black_box(text).parse::<VersionReq>().unwrap());
+    });
 }
 
-#[bench]
-fn parse_version(b: &mut Bencher) {
-    let text = "1.0.2021-beta+exp.sha.5114f85";
-    b.iter(|| hint::black_box(text).parse::<Version>().unwrap());
-}
-
-#[bench]
-fn parse_version_req(b: &mut Bencher) {
-    let text = ">=1.2.3, <2.0.0";
-    b.iter(|| hint::black_box(text).parse::<VersionReq>().unwrap());
-}
+criterion_group!(benches, bench);
+criterion_main!(benches);
